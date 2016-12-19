719364a5bb9f175e12535a98d6d34da288cb9d33
Hannah <hannahjmccain@gmail.com>
2016-12-19 16:44:28 -0500

    New server setup.

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,13 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WelcomePage from './components/welcome_page';
+
+document.addEventListener('DOMContentLoaded', () => {
+	return ReactDOM.render(
+		<WelcomePage />,
+		document.getElementById('root')
+	);
+});
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-import 'babel-polyfill';
-import express from 'express';
+require('babel-polyfill');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -7,6 +8,20 @@ const PORT = process.env.PORT || 8080;
 console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
 const app = express();
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    database: 'intimini'
+  },
+});
+
+app.use(bodyParser.json());
+
+// app.get('/users', (req, res) => {
+// 	knex('users').select('id', 'username', 'password').then((users) => {
+// 		return res.json({users});
+// 	});
+// });
 
 app.use(express.static(process.env.CLIENT_PATH));
 
