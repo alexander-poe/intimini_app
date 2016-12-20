e8db4411e184669078080dca2fe76d8a80a24f0f
Hannah <hannahjmccain@gmail.com>
2016-12-20 14:06:57 -0500

    Homepage header mockup in place.

--- client/js/components/entries_header.js
+++ client/js/components/entries_header.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
 export default function EntriesHeader(props) {
+	const changeFilter = (event) => {
+		let filterVal = this.selection.value;
+		console.log(filterVal);
+	}
+
 	return (
-		<p>Yo machine</p>
+		<div className="menu-bar">
+			<form>
+				<button className="back-to-top">Back to top</button>
+			</form>
+			<hr />
+			<form>
+				<select>
+					<option value="mood">Mood</option>
+					<option value="happy">Happy</option>
+					<option value="sad">Sad</option>
+					<option value="bored">Bored</option>
+				</select>
+				<button onClick={changeFilter}>Filter</button>
+			</form>
+			<hr />
+			<form>
+				<input type="text" placeholder="Search past entries" />
+				<button>Search</button>
+			</form>
+			<hr />
+		</div>
 	)
 }
--- client/js/components/home_page.js
+++ client/js/components/home_page.js
@@ -18,7 +18,7 @@ export default class HomePage extends React.Component {
 							date: 12182016,
 							mood: "happy",
 							content: "Today I'm happy!"
-						}
+						},
 					]
 				},
 				{
@@ -34,6 +34,31 @@ export default class HomePage extends React.Component {
 							date: 12182016,
 							mood: "sad",
 							content: "Actually meh distillery humblebrag you probably haven't heard of them, hella ad 8-bit typewriter subway tile copper mug odio meggings tempor chillwave. Anim 90's food truck slow-carb cronut. Polaroid authentic franzen, in meggings fam biodiesel letterpress placeat woke quis food truck health goth. Do banjo copper mug, mlkshk flexitarian subway tile kombucha DIY ex. Direct trade neutra twee vegan air plant tattooed knausgaard plaid, brunch sriracha selfies 3 wolf moon id stumptown. Roof party gentrify bushwick laboris, fugiat meditation fap marfa you probably haven't heard of them sriracha man braid. Brooklyn lomo chillwave kinfolk, umami neutra narwhal mumblecore kombucha cupidatat wolf godard."
+						},
+						{
+							date: 12172016,
+							mood: "happy",
+							content: "Kickstarter pariatur occupy jianbing. Enamel pin consectetur gentrify yr vexillologist, +1 bitters asymmetrical. Salvia kitsch small batch waistcoat fap dolor. Seitan pop-up elit everyday carry aliqua sed normcore, raw denim gastropub synth art party mustache street art cupidatat. Selvage tacos shoreditch whatever qui, schlitz officia laboris. Shoreditch distillery anim vape artisan raclette, jianbing asymmetrical organic tumeric hashtag yuccie. Vice selvage poke, chambray tumblr hammock exercitation pariatur vegan sustainable pickled pug kinfolk normcore."
+						},
+						{
+							date: 12162016,
+							mood: "sad",
+							content: "Nulla man bun af hot chicken labore meditation, literally blog sriracha lomo sustainable sed nesciunt. Everyday carry aliquip veniam, listicle unicorn taxidermy meditation. Green juice lomo sartorial, eiusmod vegan sed hammock locavore selfies williamsburg. Pitchfork kogi fugiat, offal raclette edison bulb blog ramps placeat. Church-key minim vero thundercats pok pok. Direct trade semiotics proident magna small batch. YOLO quinoa retro, est pug non cardigan ennui proident flexitarian fashion axe vero truffaut chartreuse."
+						},
+						{
+							date: 12162016,
+							mood: "elated",
+							content: "Yuccie excepteur gochujang, schlitz voluptate vinyl authentic four loko. Salvia irony kogi, lumbersexual nulla ex cornhole forage enamel pin craft beer neutra celiac sunt minim. Organic ullamco copper mug pinterest. Tacos heirloom occaecat, irure offal schlitz odio synth blue bottle vegan stumptown. Freegan church-key pork belly iPhone, gastropub celiac before they sold out listicle. Try-hard coloring book irure, raw denim kale chips marfa master cleanse ennui dolore wolf single-origin coffee PBR&B gochujang enamel pin. Air plant art party adipisicing pour-over."
+						},
+						{
+							date: 12152016,
+							mood: "depressed",
+							content: "La croix iPhone cronut excepteur. Hammock aliquip assumenda, occupy accusamus retro truffaut nesciunt proident cardigan fingerstache. Crucifix deep v microdosing, art party sed reprehenderit schlitz organic affogato marfa cornhole four dollar toast. Ethical vice trust fund magna. Freegan post-ironic cardigan blue bottle. Viral godard eu woke veniam, tofu williamsburg small batch scenester anim pok pok. Sartorial nesciunt gochujang, vice yuccie meh gastropub beard butcher."
+						},
+						{
+							date: 12142016,
+							mood: "bored",
+							content: "Pabst iceland pop-up fap. Enamel pin raclette unicorn, kogi austin ennui yuccie prism PBR&B non laboris synth. Cupidatat accusamus anim DIY, retro intelligentsia voluptate ea succulents in. Kitsch deserunt consectetur, ea tumeric vaporware +1 celiac duis man braid labore. Sed odio wayfarers nostrud typewriter, readymade chambray beard sustainable. Dolore small batch scenester, sint waistcoat dolor flannel nisi tilde keytar copper mug fixie artisan. Whatever cred vexillologist, tumeric meh lo-fi green juice pabst nisi."
 						}
 					]
 				}
@@ -49,7 +74,6 @@ export default class HomePage extends React.Component {
 			</div>
 		)
 	}
-
 }
 
 // const mapStateToProps = (state, props) => ({
