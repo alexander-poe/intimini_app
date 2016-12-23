bf00a276d73f5ef3928c165726a5dcc70fd581e2
Hannah <hannahjmccain@gmail.com>
2016-12-23 18:30:13 -0500

    CSS added

--- client/js/components/error_display.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ErrorDisplay () {
-	return (
-		<h4>
-			
-		</h4>
-	)
-}
--- client/js/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers/reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(reducer, composeEnhancers(
-	applyMiddleware(thunk)
-));
